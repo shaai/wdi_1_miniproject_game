075f63b9dcb2f38bfd4c423fafd08f8f752e59b5
shaai <shaai.sattar@gmail.com>
2014-09-28 19:19:21 -0400

    character generation and storing key/values in hash works

--- character_generator_game.rb
+++ character_generator_game.rb
@@ -17,46 +17,98 @@ require 'pry'
 # name1 = gets.chomp
 # puts "Good! Now we "
 
-class Fighter
 
-  def Initialize(name)
-    @name = name
-    @power_number = rand(10)
-  end
+# def character_type(name,type)
+#   slayer_set[:name] = type
+#   if type == "elf"
+#     slayer_set << 3
+#   elsif type =
+# end
 
-end
+slayer_set = {}
 
 
-
-puts "___________Welcome to BATTLE WARRIORS!___________"
-puts "Create a new warrior to start a battle."
-puts "Let's start by giving your warrior a name:"
-warrior_one = gets.chomp.upcase
-
-puts "Thank you! We now have warrior #{warrior_one} ready to fight."
-puts "Would you like #{warrior_one} to fight against the computer or another warrior?"
-puts "Type 'computer' or 'warrior' to continue:"
-answer = gets.chomp.downcase
-
-if answer == "computer"
-  puts "You chose to fight against the computer! Let the BATTLE begin!"
-
-  new_warrior_one = Fighter.new(warrior_one)
-  computer = Fighter.new("COMPUTER")
-
-elsif answer == "warrior"
-  puts "You chose to fight against another warrior."
-  puts "Let's create a second warrior by giving him/her a name. Type his/her name below:"
-  warrior_two = gets.chomp.upcase
-  puts "Warrior #{warrior_two} is ready to fight! Let the battle between #{warrior_one} and #{warrior_two} begin!!!!"
-
-  new_warrior_one = Fighter.new(warrior_one)
-  new_warrior_two = Fighter.new(warrior_two)
-
+def fight_power(type)
+  if type == "elf"
+    3
+  elsif type == "dwarf"
+    2
   else
-    puts "Something went wrong. Please start the game again."
+    1
+  end
 end
 
+
+# def put_into_hash(name,type)
+#     slayer_set.store(name.to_sym, type)
+# end
+
+# class Fighter
+#   attr_accessor :slayer_set
+
+#   def initialize(name, type)
+#     @slayer_set = {}
+#     @name = name.to_sym
+#     @type = type.to_i
+#   end
+
+
+#   def put_into_hash(name,type)
+#     @slayer_set.store(name.to_sym, type)
+#   end
+
+# end
+
+
+
+# def battle
+#   while @power_number > 0
+#     @power -= rand(5..10)
+#   end
+# end
+
+
+
+
+puts "___________Welcome to DRAGON SLAYERS!___________"
+puts "Create a clan of Dragon Slayers to fight against the Dragon."
+puts "Let's add slayers to your clan."
+puts "What's your first Dragon Slayer's name?"
+slayer_name = gets.chomp.upcase
+puts "Your slayer can be an elf, human, or dwarf. Which is of these three will #{slayer_name} be?"
+type = gets.chomp.downcase
+slayer_type = fight_power(type)
+
+# slayer = Fighter.new(slayer_name, slayer_type)
+slayer_set[slayer_name.to_sym] = slayer_type
+
+binding.pry
+
+# puts "Thank you! We now have warrior #{warrior_one} ready to fight."
+# puts "Would you like #{warrior_one} to fight against the computer or another warrior?"
+# puts "Type 'computer' or 'warrior' to continue:"
+# answer = gets.chomp.downcase
+
+# if answer == "computer"
+#   puts "You chose to fight against the computer! Let the BATTLE begin!"
+
+#   new_warrior_one = Fighter.new(warrior_one)
+#   computer = Fighter.new("COMPUTER")
+
+# elsif answer == "warrior"
+#   puts "You chose to fight against another warrior."
+#   puts "Let's create a second warrior by giving him/her a name. Type his/her name below:"
+#   warrior_two = gets.chomp.upcase
+#   puts "Warrior #{warrior_two} is ready to fight! Let the battle between #{warrior_one} and #{warrior_two} begin!!!!"
+
+#   new_warrior_one = Fighter.new(warrior_one)
+#   new_warrior_two = Fighter.new(warrior_two)
+
+#   else
+#     puts "Something went wrong. Please start the game again."
+# end
+
+# puts ""
 
 
 
